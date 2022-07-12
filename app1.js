104d275fda27b13d91f67791210ca78d6b2c32c0
danielfajuyi <ayomidedaniels121@gmail.com>
2022-07-13 00:57:04 +0100

    first commit

---
+++ app1.js
@@ -0,0 +1,17 @@
+//Modules - Encapsulated Code (only share minimum)
+// CommonJs, every file is a modle (by default)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+ require('./7-mind-grenade')
+
+
+
+    sayHi('susan')
+    sayHi(names.john)
+    sayHi(names.peter)
+ console.log(data)
+
+
+
+
