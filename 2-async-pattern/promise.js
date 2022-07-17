44e5c29901a803b98f6b4061ec02df7cc7c70e81
danielfajuyi <ayomidedaniels121@gmail.com>
2022-07-17 18:24:16 +0100

    third commit

---
+++ 2-async-pattern/promise.js
@@ -0,0 +1,18 @@
+const {readFile} = require('fs');
+
+const getText = (path) => {
+    return new Promise((resolve,reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+}
+
+getText('./content/subfolder/text.txt')
+.then((result) => console.log(result))
+.catch((err) => console.log(err)) 
--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const {readFile} = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
 
-readFile('./content/first.txt', 'utf8', (err, data) => {
-    if (err) {
-        return
-    }
-    else {
-        console.log(data)
-    }
-})
+const getText = (path) => {
+    return new Promise((resolve,reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+};
 
+
+
+const start = async() => {
+    try {
+        const first = await getText ('./content/first.txt');
+        const second = await getText ('./content/second.txt');
+        console.log(first, second);
+    } 
+    catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err)) 
